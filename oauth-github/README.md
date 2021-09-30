bd4fb0e1842b0a5b737923533b358e094c5273c5
Ken Moini <ken@kenmoini.com>
2021-09-29 20:50:29 -0400

    add github stuffs

---
+++ oauth-github/README.md
@@ -0,0 +1,178 @@
+# GitHub OAuth Identity Provider for OpenShift
+
+# Automation
+
+*(Still needs the Manual Processes > Prerequisites to take place in GitHub)*
+
+In this directory you can find a `./configure.sh` file that will allow you to quickly apply GitHub OAuth authentication to a logged in cluster.  ***Edit the file*** and run as follows:
+
+```bash
+# Export the client credential variables
+export GITHUB_CREDENTIAL_CLIENT_ID="abc123"
+export GITHUB_CREDENTIAL_CLIENT_SECRET="someLongSecretString"
+# See `./configure.sh` for extra variables
+
+# Check configuration
+./configure.sh
+# Actually apply configuration
+./configure.sh --commit
+```
+
+Requires the GitHub OAuth Client Credentials to be passed as environmental variables.
+
+# Manual Processes
+
+## Prerequisites
+
+Before you can configure OpenShift to use GitHub as an Identity Provider, you need to create the OAuth Client in GitHub.
+
+### 1. Navigate to GitHub Settings
+
+<div align="center" style="text-align:center">
+
+<img alt="Goto your user's GitHub Settings" src="./img/1.png" style="border:2px solid #000" />
+
+**Goto your user's GitHub Settings**
+
+</div>
+
+### 2. Navigate to Developer Settings
+
+<div align="center" style="text-align:center">
+
+<img alt="Use the left-hand pane to navigate to Developer Settings" src="./img/2.png" style="border:2px solid #000" />
+
+**Use the left-hand pane to navigate to Developer Settings**
+
+</div>
+
+### 3. Navigate to OAuth Apps
+
+<div align="center" style="text-align:center">
+
+<img alt="Use the left-hand pane to navigate to OAuth Apps" src="./img/3.png" style="border:2px solid #000" />
+
+**Use the left-hand pane to navigate to OAuth Apps**
+
+</div>
+
+### 4. Create a New OAuth App
+
+<div align="center" style="text-align:center">
+
+<img alt="Click 'New OAuth App'" src="./img/4.png" style="border:2px solid #000" />
+
+**Click 'New OAuth App'**
+
+</div>
+
+### 5. Provide Information
+
+<div align="center" style="text-align:center">
+
+<img alt="Fill in the information to match your OpenShift cluster and other information" src="./img/5.png" style="border:2px solid #000" />
+
+**Provide the GitHub OAuth Application some primer information**
+
+</div>
+
+Provide a:
+
+- Application Name, whatever you'd like - this will show on the Authorization flow screen
+- A URL - this isn't checked and doesn't really matter outside of metadata
+- A Description, maybe
+- The Authorization Callback URL is the most important part of this form - it'll be the OpenShift OAuth Callback endpoint, eg `https://oauth-openshift.apps.cluster-3078.3078.sandbox601.opentlc.com/oauth2callback/google` following the format of: `https://oauth-openshift.apps.<cluster_name>.<base_domain>/oauth2callback/<IdP_Name>`
+
+Next, click **Register application**
+
+### 6. Generate a new Client Secret
+
+<div align="center" style="text-align:center">
+
+<img alt="Click the 'Generate a new client secret' button" src="./img/6.png" style="border:2px solid #000" />
+
+**Click the 'Generate a new client secret' button**
+
+</div>
+
+With the OAuth Client Application registered, you are provided a Client ID - this is static and assigned to this application with the only way to change the Client ID is to create a new OAuth Application.
+
+A Client Secret is still needed - click the **Generate a new client secret button**
+
+### 7. Copy Client ID and Client Secret
+
+<div align="center" style="text-align:center">
+
+<img alt="The Client ID and Client Secret are needed moving forward" src="./img/7.png" style="border:2px solid #000" />
+
+**Take note of the Client ID and Client Secret**
+
+</div>
+
+The Client ID and Client Secret are needed moving forward.  If this is a private instance of GitHub Enterprise, the CA Certificate PEM will also be needed.
+
+## Adding the GitHub IdP to the OpenShift Cluster
+
+With the configuration finished in GitHub, we can now add our OAuth information to the cluster for consumption.
+
+## 1. Create OpenShift Secret for the OAuth Client Secret
+
+Next, you can create the needed OpenShift Secret with the following command, so long as you are logged in as a cluster-admin
+
+```bash
+oc create secret generic github-oauth-client-secret --from-literal=clientSecret=$GITHUB_CREDENTIAL_CLIENT_SECRET -n openshift-config
+```
+
+## 2. [Optional] Create an OpenShift ConfigMap for the GitHub Enterprise CA Certificate
+
+If you're connecting to a private GitHub Enterprise instance, OpenShift will need the CA Certificate chain to validate connections.  Provide that with the following command:
+
+```bash
+oc create configmap github-ca-config-map --from-file=ca.crt=$GITHUB_CA_CERT_PEM_FILE -n openshift-config
+```
+
+## 3. Create the YAML for the OAuth Custom Resource
+
+With the Secret created, you can now reference it in the YAML definition of the OAuth provider:
+
+https://docs.openshift.com/container-platform/4.8/authentication/identity_providers/configuring-github-identity-provider.html#identity-provider-github-CR_configuring-github-identity-provider
+
+- Replace the ***CLIENT_ID_HERE*** text in the following YAML with whatever your Client ID is:
+
+```yaml
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - name: github
+    mappingMethod: claim
+    type: GitHub
+    github:
+      clientID: CLIENT_ID_HERE
+      clientSecret:
+        name: github-oauth-client-secret
+```
+
+***Notes:***
+
+- The `.spec.identityProviders[0].name` is the same as the IdP Name prefixed to the end of the Authorized Redirect URI that was created in GitHub OAuth Application Client Registration earlier
+- There are other options available to specify such as Organization and Team filters
+- If this is a private GitHub Enterprise instance then you also need to define the `.spec.identityProviders[0].github.ca.name` parameter
+
+## 4. Apply the YAML to the OpenShift Cluster
+
+With the YAML created, we can apply it to the cluster now with the following command, assuming you saved it to a file called oauth.yaml:
+
+> ***WARNING*** The following command may overwrite your current IdP settings!
+
+```bash
+oc patch OAuth cluster --patch-file oauth.yaml
+```
+
+## 5. ???????
+
+## 6. PROFIT!!!!!1
+
+Once the Authentication Operator has restarted you should be able to log into the cluster with GitHub Authentication!
---
+++ oauth-github/configure.sh
@@ -0,0 +1,259 @@
+#!/bin/bash
+
+#######################################################################
+# Setup Vars
+#######################################################################
+
+## GITHUB_CREDENTIAL_CLIENT_ID & GITHUB_CREDENTIAL_CLIENT_SECRET need to be defined
+export GITHUB_CREDENTIAL_CLIENT_ID=${GITHUB_CREDENTIAL_CLIENT_ID:=""}
+export GITHUB_CREDENTIAL_CLIENT_SECRET=${GITHUB_CREDENTIAL_CLIENT_SECRET:=""}
+
+## GITHUB_ORGANIZATIONS is the limit on what GitHub Organizations can use this to log in - separated by a semicolon ';'
+export GITHUB_ORGANIZATIONS=${GITHUB_ORGANIZATIONS:=""}
+
+## GITHUB_TEAMS is the limit on what GitHub Organization Teams can use this to log in - separated by a semicolon ';'
+export GITHUB_TEAMS=${GITHUB_TEAMS:=""}
+
+## GITHUB_HOSTNAME should only be used if this is a private hosted GitHub Enterprise instance being targeted
+export GITHUB_HOSTNAME=${GITHUB_HOSTNAME:=""}
+
+## GITHUB_CA_CERT_PEM_FILE is the location to the CA Certificate the GitHub Enterprise server is signed by
+export GITHUB_CA_CERT_PEM_FILE=${GITHUB_CA_CERT_PEM_FILE:=""}
+
+#######################################################################
+# Functions
+#######################################################################
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=linux;;
+    Darwin*)    machine=osx-amd;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+case "${unameOut}" in
+    Linux*)     yqmachine=linux;;
+    Darwin*)    yqmachine=darwin;;
+    *)          yqmachine="UNKNOWN:${unameOut}"
+esac
+
+archOut="$(arch)"
+case "${archOut}" in
+    x86_64*)     arch=64;;
+    x86*)        arch=32;;
+    *)          arch="UNKNOWN:${archOut}"
+esac
+case "${archOut}" in
+    x86_64*)     yqarch=amd64;;
+    x86*)        yqarch=386;;
+    *)          arch="UNKNOWN:${archOut}"
+esac
+
+
+PWD=$(pwd)
+PARENT_DIR=$(dirname "$PWD")
+BIN_DIR="${PARENT_DIR}/bin"
+JQ_BIN="${BIN_DIR}/jq"
+YQ_BIN="${BIN_DIR}/yq"
+
+function checkForProgramAndExit() {
+    command -v $1
+    if [[ $? -eq 0 ]]; then
+        printf '%-72s %-7s\n' $1 "PASSED!";
+    else
+        printf '%-72s %-7s\n' $1 "FAILED!";
+        exit 1
+    fi
+}
+
+function containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+function checkjq () {
+  mkdir -p $BIN_DIR
+
+  if [ ! -f "${BIN_DIR}/jq" ]; then
+    curl -sSL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-${machine}${arch} -o "${BIN_DIR}/jq"
+  fi
+
+  chmod +x "${BIN_DIR}/jq"
+}
+checkjq
+
+function checkyq () {
+  mkdir -p $BIN_DIR
+
+  if [ ! -f "${BIN_DIR}/yq" ]; then
+    curl -sSL https://github.com/mikefarah/yq/releases/download/v4.13.2/yq_${yqmachine}_${yqarch} -o "${BIN_DIR}/yq"
+  fi
+
+  chmod +x "${BIN_DIR}/yq"
+}
+checkyq
+
+#######################################################################
+# Preflight
+#######################################################################
+
+echo "===== Checking for required applications..."
+export PATH="${BIN_DIR}:$PATH"
+
+checkForProgramAndExit oc
+checkForProgramAndExit jq
+checkForProgramAndExit yq
+
+if [[ -z "$GITHUB_CREDENTIAL_CLIENT_ID" ]] || [[ -z "$GITHUB_CREDENTIAL_CLIENT_SECRET" ]]; then
+  echo "GitHub OAuth Credentials not found at \$GITHUB_CREDENTIAL_CLIENT_ID and \$GITHUB_CREDENTIAL_CLIENT_SECRET!"
+  echo "Failed preflight!"
+  exit 1
+else
+  CLIENT_ID=$GITHUB_CREDENTIAL_CLIENT_ID
+  CLIENT_SECRET=$GITHUB_CREDENTIAL_CLIENT_SECRET
+fi
+
+GITHUB_ORG_ARRAY=()
+GITHUB_TEAMS_ARRAY=()
+
+if [[ ! -z $GITHUB_ORGANIZATIONS ]]; then
+  GITHUB_ORG_ARRAY=(${GITHUB_ORGANIZATIONS//;/ })
+fi
+
+if [[ ! -z $GITHUB_TEAMS ]]; then
+  GITHUB_TEAMS_ARRAY=(${GITHUB_TEAMS//;/ })
+fi
+
+if [[ ! -z $GITHUB_CA_CERT_PEM_FILE ]]; then
+  if [[ ! -f $GITHUB_CA_CERT_PEM_FILE ]]; then
+    echo "GitHub CA Certificate defined as \$GITHUB_CA_CERT_PEM_FILE but not found on the filesystem!"
+    echo "Failed preflight!"
+    exit 1
+  fi
+fi
+
+# Dry run mode
+if [[ ! -z $1 ]]; then
+  if [[ $1 != "--commit" ]]; then
+      echo -e "\n======================================================================\nDry run - configuration NOT applied to cluster!  Rerun with '--commit'\n======================================================================\n"
+  fi
+else
+  echo -e "\n======================================================================\nDry run - configuration NOT applied to cluster!  Rerun with '--commit'\n======================================================================\n"
+fi
+
+#######################################################################
+# Main Script
+#######################################################################
+
+# Check for a logged in user
+oc whoami >/dev/null 2>&1
+if [[ $? == 0 ]]; then
+
+  # Check for existing secret
+  oc get secret github-oauth-client-secret -n openshift-config >/dev/null 2>&1
+  if [[ $? == 1 ]]; then
+    if [[ $1 == "--commit" ]]; then
+      echo "===== Creating GitHub OAuth Client Secret, Secret..."
+      oc create secret generic github-oauth-client-secret --from-literal=clientSecret=$CLIENT_SECRET -n openshift-config
+    else
+      echo -e "\n===== Target YAML Modification:\n"
+      oc create secret generic github-oauth-client-secret --from-literal=clientSecret=$CLIENT_SECRET -n openshift-config --dry-run=client -o yaml
+    fi
+  fi
+
+  # Check for existing CA Cert ConfigMap if needed
+  if [[ ! -z $GITHUB_CA_CERT_PEM_FILE ]] && [[ -f $GITHUB_CA_CERT_PEM_FILE ]]; then
+    # Check for existing CA Cert ConfigMap
+    oc get configmap github-ca-config-map -n openshift-config >/dev/null 2>&1
+    if [[ $? == 1 ]]; then
+      if [[ $1 == "--commit" ]]; then
+        echo "===== Creating GitHub CA Cert ConfigMap..."
+        oc create configmap github-ca-config-map --from-file=ca.crt=$GITHUB_CA_CERT_PEM_FILE -n openshift-config
+      else
+        echo -e "\n===== Target YAML Modification:\n"
+        oc create configmap github-ca-config-map --from-file=ca.crt=$GITHUB_CA_CERT_PEM_FILE -n openshift-config --dry-run=client -o yaml
+      fi
+    fi
+  fi
+
+  # Template out the OAuth Config
+  sed "s|CLIENT_ID_HERE|${CLIENT_ID}|g" oauth-config.yaml.template > oauth-config.yaml
+
+  echo "" >> oauth-config.yaml
+  # Add Hostname
+  if [[ ! -z "$GITHUB_HOSTNAME" ]]; then
+    echo "      hostname: ${GITHUB_HOSTNAME}" >> oauth-config.yaml
+  fi
+  # Add CA Certificate definition
+  if [[ ! -z $GITHUB_CA_CERT_PEM_FILE ]] && [[ -f $GITHUB_CA_CERT_PEM_FILE ]]; then
+    echo "      ca:" >> oauth-config.yaml
+    echo "        name: github-ca-config-map" >> oauth-config.yaml
+  fi
+  # Add Organization Filters
+  if [[ ${#GITHUB_ORG_ARRAY[@]} -gt 0 ]]; then
+    echo "      organizations:" >> oauth-config.yaml
+    for org in ${GITHUB_ORG_ARRAY[@]}; do
+      echo "      - ${org}" >> oauth-config.yaml
+    done
+  fi
+  # Add Team Filters
+  if [[ ${#GITHUB_TEAMS_ARRAY[@]} -gt 0 ]]; then
+    echo "      teams:" >> oauth-config.yaml
+    for team in ${GITHUB_TEAMS_ARRAY[@]}; do
+      echo "      - ${team}" >> oauth-config.yaml
+    done
+  fi
+  
+  # Take current OAuth cluster configuration
+  CURRENT_CLUSTER_CONFIG=$(oc get OAuth cluster -o yaml)
+  CURRENT_CONFIG_LEN=$(echo "$CURRENT_CLUSTER_CONFIG" | ${YQ_BIN} eval '.spec.identityProviders | length' -)
+  CURRENT_IDPs=""
+  CURRENT_IDP_NAMES=()
+  echo -e "\n===== Current Config (${CURRENT_CONFIG_LEN}):\n\n${CURRENT_CLUSTER_CONFIG}"
+
+  # Add current OAuth Identity Providers to an array
+  for ((n=0;n<$CURRENT_CONFIG_LEN;n++))
+  do
+    CUR_NAME=$(echo "$CURRENT_CLUSTER_CONFIG" | ${YQ_BIN} eval '.spec.identityProviders['$n'].name' -)
+    echo "===== Found IDP: ${CUR_NAME}..."
+    CURRENT_IDP_NAMES=(${CURRENT_IDP_NAMES[@]} "$CUR_NAME")
+    CURRENT_IDPs="${CURRENT_IDPs}$(echo "$CURRENT_CLUSTER_CONFIG" | ${YQ_BIN} -o=json eval '.spec.identityProviders['$n']' -),"
+  done
+
+  containsElement $(cat oauth-config.yaml | ${YQ_BIN} eval '.spec.identityProviders[0].name' -) "${CURRENT_IDP_NAMES[@]}"
+
+  if [[ $? == 0 ]]; then
+    echo "This identity provider $(cat oauth-config.yaml | ${YQ_BIN} eval '.spec.identityProviders[0].name' -) looks to already be configured!"
+    exit 0
+  else
+    # Add the proposed IDP to the array
+    CURRENT_IDPs="[${CURRENT_IDPs}$(cat oauth-config.yaml | ${YQ_BIN} -o=json eval '.spec.identityProviders[0]' -)]"
+
+    # Apply the joined configuration
+    echo "Adding GitHub OAuth to OAuth cluster configuration..."
+    PATCH_CONTENTS='{"spec": { "identityProviders": '${CURRENT_IDPs}' }}'
+    if [[ $1 == "--commit" ]]; then
+      echo "Writing configuration to cluster!"
+      oc patch OAuth cluster --type merge --patch "$PATCH_CONTENTS"
+    else
+      echo -e "\n===== Target YAML Modification:\n"
+      oc patch OAuth cluster --type merge --patch "$PATCH_CONTENTS" --dry-run=client -o yaml
+    fi
+  fi
+
+  # Dry run mode
+  if [[ ! -z $1 ]]; then
+    if [[ $1 != "--commit" ]]; then
+        echo -e "\n======================================================================\nDry run - configuration NOT applied to cluster!  Rerun with '--commit'\n======================================================================\n"
+    fi
+  else
+    echo -e "\n======================================================================\nDry run - configuration NOT applied to cluster!  Rerun with '--commit'\n======================================================================\n"
+  fi
+
+  echo -e "\nFinished provisioning GitHub OAuth Identity Provider for OpenShift!\n\n"
+  exit 0
+else
+  echo "Not logged into an OpenShift cluster with `oc` CLI!"
+  exit 1
+fi
